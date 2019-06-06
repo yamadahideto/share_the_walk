821c6f88b3e86d7b2813d244fa499c545502ee20
yamadahideto <yamadahideto@yamadahidetonoMacBook-Pro.local>
2019-06-06 10:26:26 +0900

    edit_message_views

---
+++ app/controllers/animals_controller.rb
@@ -0,0 +1,24 @@
+class AnimalsController < ApplicationController
+  def index
+    @animal = Animal.order("id ASC")
+  end
+
+  def new
+    @animal = Animal.new
+  end
+  def create
+    @animal = Animal.new(create_params)
+    @animal.save!
+    redirect_to root_path
+  end
+
+
+  def show
+    @animal = Animal.find_by(id: 1)
+  end
+
+  private
+  def create_params
+    params.require(:animal).permit(:image, :name, :kind, :sex, :age, :time, :course, :notes)
+  end
+end
